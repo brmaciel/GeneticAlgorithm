ebf3152a67de783a1eae9b407904b054e5a2bff8
Bruno Maciel <brunomaciel.xc4@gmail.com>
2019-01-03 18:09:12 -0200

    Add metodos de crossover para problemas binarios

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,4 +1,5 @@
 from random import randint
+from math import ceil, floor
 
 
 class GeneticAlgorithm(object):
@@ -53,6 +54,89 @@ class GeneticAlgorithm(object):
         #print("Pontuacao:", self.__adaptabilityLevel)
         print("Rank:", chromosomeRank)
         return chromosomeRank
+
+
+    # Funcoes de CRUZAMENTO entre cromossomos
+    def __crossover(self, chromosome1, chromosome2):
+        # Inicializa o processo de Crossover(cruzamento)
+        if self.__crossoverMethod == 'two-points':
+            newChromosome1, newChromosome2 = self.__two_points_crossover(chromosome1, chromosome2)
+        else:
+            newChromosome1, newChromosome2 = self.__single_point_crossover(chromosome1, chromosome2)
+
+        return newChromosome1, newChromosome2
+
+    @staticmethod
+    def __single_point_crossover(chromossomeA, chromossomeB):
+        # Utiliza o metodo SinglePoint, dividindo os cromossomos na metade
+        # ex: [a,b,c,d, | e,f,g,h] para tamanho de cromossomos pares
+        # ex: [a,b,c,d, | e,f,g] para tamanho de cromossomos impares
+        # print("Single-Point Crossover Method")
+        newChromossomeA = []
+        newChromosomeB = []
+        tamanho = len(chromossomeA)
+        middle = ceil(tamanho / 2)
+
+        # primeira metade do cromossomo permanece igual
+        for i in range(middle):
+            newChromossomeA.append(chromossomeA[i])
+            newChromosomeB.append(chromossomeB[i])
+
+        # segunda metade do cromossomo eh alterada
+        # se tamanho do cromosso for impar, segunda metade tem tem um bit a menos que a primeira metade
+        if len(chromossomeA) % 2 == 0:
+            for i in range(middle, tamanho):
+                newChromossomeA.append(chromossomeB[i])
+                newChromosomeB.append(chromossomeA[i])
+        else:
+            for i in range(middle, tamanho):
+                newChromossomeA.append(chromossomeB[i])
+                newChromosomeB.append(chromossomeA[i])
+
+        return newChromossomeA, newChromosomeB
+
+    @staticmethod
+    def __two_points_crossover(chromosomeA, chromosomeB):
+        # Utiliza o metodo TwoPoint, dividindo os cromossomos em 3 partes, e trocando a informacao do meio
+        # ex: [a b c | d e | f g h] para tamanho de cromossomos pares
+        # ex: [a b | c d e | f g]   para tamanho de cromossomos impares
+        # ex: [a | b | c]           para tamanho de cromossomos == 3 (caso especial)
+        print("Two-Points Crossover Method")
+        newChromosomeA = []
+        newChromosomeB = []
+        tamanho = len(chromosomeA)
+        initialPoint = floor(tamanho / 2) - 1
+
+        # primeira parte do cromossomo permanece igual
+        for i in range(initialPoint):
+            newChromosomeA.append(chromosomeA[i])
+            newChromosomeB.append(chromosomeB[i])
+
+        # segunda parte do cromossomo eh alterada
+        if tamanho % 2 == 0:
+            endPoint = initialPoint + 1
+            for i in range(initialPoint, endPoint + 1):
+                newChromosomeA.append(chromosomeB[i])
+                newChromosomeB.append(chromosomeA[i])
+        elif tamanho == 3:
+            endPoint = initialPoint + 1
+            newChromosomeA.append(chromosomeA[0])
+            newChromosomeB.append(chromosomeB[0])
+            newChromosomeA.append(chromosomeB[1])
+            newChromosomeB.append(chromosomeA[1])
+        else:
+            endPoint = initialPoint + 2
+            for i in range(initialPoint, endPoint + 1):
+                newChromosomeA.append(chromosomeB[i])
+                newChromosomeB.append(chromosomeA[i])
+
+        # terceira parte do cromossomo permanece igual
+        for i in range(endPoint + 1, tamanho):
+            newChromosomeA.append(chromosomeA[i])
+            newChromosomeB.append(chromosomeB[i])
+
+        return newChromosomeA, newChromosomeB
+    # end crossover
 
     ###     Metodos Auxiliares     ###
     def __printPopulation(self):
